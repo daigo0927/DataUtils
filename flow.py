2565ede710735f8cc373ef59c9f63ecbc511984a
Daigo Hirooka <daigo@gorgon.murata.eb.waseda.ac.jp>
2018-10-30 21:20:53 +0900

    add FlyingThings3D dataset pipeline

--- flow.py
+++ flow.py
@@ -151,18 +151,82 @@ class FlyingChairs(BaseDataset):
         self.split(samples)
 
 
-# FlyingThings
+# FlyingThings3D
 # ============================================================
 class FlyingThings3D(BaseDataset):
-    def __init__(self, dataset_dir, train_or_val = 'train', origin_size = None,
-                 crop_type = 'random', crop_shape = None,
+    def __init__(self, dataset_dir, train_or_val = 'train', flow_type = 'for',
+                 origin_size = None, crop_type = 'random', crop_shape = None,
                  resize_shape = None, resize_scale = None):
+        if flow_type not in ['for', 'bi']:
+            raise KeyError(f'flow_type must be either for/bi, but {flow_type} is passed')
+        self.flow_type = flow_type
         super().__init__(dataset_dir, train_or_val, origin_size,
                          crop_type, crop_shape, resize_shape, resize_scale)
 
+    def __getitem__(self, idx):
+        img0_path, img1_path, for_path, back_path = self.samples[idx]
+        image_0, image_1 = map(imageio.imread, (img0_path, img1_path))
+        flow_for, flow_back = map(self.load_flow, (for_path, back_path))
+        
+        if self.crop_shape is not None:
+            cropper = utils.StaticRandomCrop(image_0.shape[:2], self.crop_shape) if self.crop_type == 'random'\
+              else utils.StaticCenterCrop(image_0.shape[:2], self.crop_shape)
+            image_0, image_1, flow_for, flow_back = map(cropper, [image_0, image_1, flow_for, flow_back])
+            
+        if self.resize_shape is not None:
+            image_0 = cv2.resize(image_0, dsize = tuple(self.resize_shape[::-1]))
+            image_1 = cv2.resize(image_1, dsize = tuple(self.resize_shape[::-1]))
+            flow_for = resize_flow(flow_for, self.resize_shape)
+            flow_back = resize_flow(flow_back, self.resize_shape)
+            
+        if self.resize_scale is not None:
+            sx, sy = self.resize_scale
+            image_0 = cv2.resize(image_0, dsize = (0, 0), fx = sx, fy = sy)
+            image_1 = cv2.resize(image_1, dsize = (0, 0), fx = sx, fy = sy)
+            flow_for = rescale_flow(flow_for, self.resize_scale)
+            flow_back = rescale_flow(flow_back, self.resize_scale)
+
+        images = np.stack([image_0, image_1], axis = 0)
+        if self.flow_type == 'for':
+            return images, flow_for
+        elif self.flow_type == 'bi':
+            return images, flow_for, flow_back
+
+    def has_txt(self):
+        p = Path(self.dataset_dir) / (self.train_or_val + '.txt')
+        self.samples = []
+        with open(p, 'r') as f:
+            for i in f.readlines():
+                img_0_path, img_1_path, for_path, back_path = i.split(',')
+                back_path = back_path.strip()
+                self.samples.append((img_0_path, img_1_path, for_path, back_path))
+
     def has_no_txt(self):
-        # TODO
-        pass
+        print('Creating file holding data paths, this may take for a while')
+        p = Path(self.dataset_dir)
+        p_img = p / 'frames_cleanpass/TRAIN'
+        p_flow = p / 'optical_flow/TRAIN'
+
+        samples = []
+        for d in ['A', 'B', 'C']:
+            p_img_d = p_img / d
+            p_flow_d = p_flow / d
+
+            p_imgs = sorted(map(str, p_img_d.glob('**/left/*.png')))
+            p_fors = sorted(map(str, p_flow_d.glob('**/into_future/left/*.pfm')))
+            p_backs = sorted(map(str, p_flow_d.glob('**/into_past/left/*.pfm')))
+            collections_img = [list(g) for k, g in groupby(p_imgs, lambda x: x.split('/')[-3])]
+            collections_for = [list(g) for k, g in groupby(p_fors, lambda x: x.split('/')[-4])]
+            collections_back = [list(g) for k, g in groupby(p_backs, lambda x: x.split('/')[-4])]
+
+            for col_img, col_for, col_back in zip(collections_img, collections_for, collections_back):
+                samples += [(*i, p_for, p_back) for i, p_for, p_back in \
+                            zip(utils.window(col_img, 2), col_for[:-1], col_back[1:])]
+        self.split(samples)
+
+    def load_flow(self, flow_path):
+        data = utils.readPFM(flow_path)
+        return data[:,:,:2]
 
     
 # Sintel
@@ -266,11 +330,12 @@ def get_dataset(dataset_name):
     Returns: dataset tf.data.Dataset: target dataset class
 
     Available dataset pipelines
-    - FlyingChairs
+    - FlyingChairs, FlyingThings3D
     - Sintel, SintelClean, SintelFinal
     - KITTI
     """
     datasets = {"FlyingChairs":FlyingChairs,
+                "FlyingThings3D":FlyingThings3D,
                 "Sintel":Sintel,
                 "SintelClean":SintelClean,
                 "SintelFinal":SintelFinal,
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import re, sys
 from itertools import islice
 import numpy as np
 import random
@@ -56,3 +57,41 @@ def get_size(origin_size = None, crop_size = None, resize_size = None, resize_sc
         image_size = (h, w)
 
     return image_size
+
+
+def readPFM(file):
+    file = open(file, 'rb')
+
+    color = None
+    width = None
+    height = None
+    scale = None
+    endian = None
+
+    header = file.readline().decode('utf-8').rstrip()
+    if header == 'PF':
+        color = True
+    elif header == 'Pf':
+        color = False
+    else:
+        raise Exception('Not a PFM file.')
+
+    dim_match = re.match(r'^(\d+)\s(\d+)\s$', file.readline().decode('utf-8'))
+    if dim_match:
+        width, height = map(int, dim_match.groups())
+    else:
+        raise Exception('Malformed PFM header.')
+
+    scale = float(file.readline().rstrip())
+    if scale < 0: # little-endian
+        endian = '<'
+        scale = -scale
+    else:
+        endian = '>' # big-endian
+
+    data = np.fromfile(file, endian + 'f')
+    shape = (height, width, 3) if color else (height, width)
+
+    data = np.reshape(data, shape)
+    data = np.flipud(data)
+    return data # , scale
